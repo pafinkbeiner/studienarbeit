172b2e4025325e789fe3da8a016c0bfa0a3c28d1
pafinkbeiner <paul-d-f@web.de>
2021-01-23 17:28:15 +0100

    changed unnecassary values

--- server/src/Handler/InjectionUnitHandler.ts
+++ server/src/Handler/InjectionUnitHandler.ts
@@ -1,24 +1,9 @@
-export let positionMax = (machineId: string) => {
-    console.log("InjectionUnit Handler", machineId)
-}
-
-export let positionMin = (machineId: string) => {
-    console.log("InjectionUnit Handler", machineId)
-}
 
 export let positionX = (machineId: string) => {
     console.log("InjectionUnit Handler", machineId)
 }
 
 export let fillingLevelLevel = (machineId: string) => {
-    console.log("InjectionUnit Handler", machineId)
-}
-
-export let fillingLevelMaxLevel = (machineId: string) => {
-    console.log("InjectionUnit Handler", machineId)
-}
-
-export let fillingLevelMinLevel = (machineId: string) => {
     console.log("InjectionUnit Handler", machineId)
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -22,30 +22,18 @@ client.on('message', function (topic, message) {
     if(mqttWildcard(topic,`machines/+/data/operation/running`) !== null) { OperationHandler.running(mqttWildcard(topic,`machines/+/data/operation/running`)); }
     if(mqttWildcard(topic,`machines/+/data/operation/automatic`) !== null) { OperationHandler.automatic(mqttWildcard(topic,`machines/+/data/operation/automatic`)); }
     // Injection Unit
-    if(mqttWildcard(topic,`machines/+/data/injectionUnit/position/max`) !== null) { InjectionUnitHandler.positionMax(mqttWildcard(topic,`machines/+/data/injectionUnit/position/max`)); }
-    if(mqttWildcard(topic,`machines/+/data/injectionUnit/position/min`) !== null) { InjectionUnitHandler.positionMin(mqttWildcard(topic,`machines/+/data/injectionUnit/position/min`)); }
     if(mqttWildcard(topic,`machines/+/data/injectionUnit/position/x`) !== null) { InjectionUnitHandler.positionX(mqttWildcard(topic,`machines/+/data/injectionUnit/position/x`)); }
     if(mqttWildcard(topic,`machines/+/data/injectionUnit/fillingLevel/level`) !== null) { InjectionUnitHandler.fillingLevelLevel(mqttWildcard(topic,`machines/+/data/injectionUnit/fillingLevel/level`)); }
-    if(mqttWildcard(topic,`machines/+/data/injectionUnit/fillingLevel/minLevel`) !== null) { InjectionUnitHandler.fillingLevelMinLevel(mqttWildcard(topic,`machines/+/data/injectionUnit/fillingLevel/minLevel`)); }
-    if(mqttWildcard(topic,`machines/+/data/injectionUnit/fillingLevel/maxLevel`) !== null) { InjectionUnitHandler.fillingLevelMaxLevel(mqttWildcard(topic,`machines/+/data/injectionUnit/fillingLevel/maxLevel`)); }
     // Savety Door 
-    if(mqttWildcard(topic,`machines/+/data/savetyDoor/position/max`) !== null) { console.log("Hey") }
-    if(mqttWildcard(topic,`machines/+/data/savetyDoor/position/min`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/savetyDoor/position/x`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/savetyDoor/locked`) !== null) { console.log("Hey") }
     // Locking Unit
-    if(mqttWildcard(topic,`machines/+/data/lockingUnit/position/max`) !== null) { console.log("Hey") }
-    if(mqttWildcard(topic,`machines/+/data/lockingUnit/position/min`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/lockingUnit/position/x`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/lockingUnit/locked`) !== null) { console.log("Hey") }
-    if(mqttWildcard(topic,`machines/+/data/lockingUnit/closingForce/maxForce`) !== null) { console.log("Hey") }
-    if(mqttWildcard(topic,`machines/+/data/lockingUnit/closingForce/minForce`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/lockingUnit/closingForce/force`) !== null) { console.log("Hey") }
     // Material Info
     if(mqttWildcard(topic,`machines/+/data/materialInfo/temp`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/materialInfo/material`) !== null) { console.log("Hey") }
-    if(mqttWildcard(topic,`machines/+/data/materialInfo/pressure/maxForce`) !== null) { console.log("Hey") }
-    if(mqttWildcard(topic,`machines/+/data/materialInfo/pressure/minForce`) !== null) { console.log("Hey") }
     if(mqttWildcard(topic,`machines/+/data/materialInfo/pressure/force`) !== null) { console.log("Hey") }
 
     console.log(`Topic: ${topic} und Message: ${message}`);
