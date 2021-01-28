78d337f8cde9466374792540f7cd6153ce987517
asd <sad>
2021-01-28 15:23:41 +0100

    added env support

--- machine/src/Helper/mqtt.ts
+++ machine/src/Helper/mqtt.ts
@@ -1,7 +1,7 @@
 import * as mqtt from "async-mqtt"
 
 // Connect to MQTT Broker
-let client  = mqtt.connect('mqtt://paul-finkbeiner.de')
+let client  = mqtt.connect(process.env.MQTT_BROKER || 'mqtt://paul-finkbeiner.de')
 client.on("connect", () => {console.log("Connected to MQTT Broker!")})
 
 export default client;
--- server/src/Helper/ws.ts
+++ server/src/Helper/ws.ts
@@ -1,11 +1,10 @@
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import http from 'http';
-import { Subject } from 'rxjs';
 
 const httpServer = http.createServer();
 const io = new SocketIOServer(httpServer);
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.WS_PORT || 7000;
 
 httpServer.listen(PORT, () => {
     console.log(`HTTP server listening on http://localhost:${PORT}`);
