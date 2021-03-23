36e74edf9faf3d16db89c854647fc4b2234baddc
pafinkbeiner <paul-d-f@web.de>
2021-03-23 08:04:41 +0100

    env change machine

--- machine/src/Helper/mqtt.ts
+++ machine/src/Helper/mqtt.ts
@@ -2,9 +2,8 @@ import * as mqtt from "async-mqtt"
 import dotenv from "dotenv"
 dotenv.config();
 
-console.log(process.env.MQTT_BROKER)
 // Connect to MQTT Broker
-let client  = mqtt.connect('mqtt://test.mosquitto.org')
+let client  = mqtt.connect(process.env.MQTT_BROKER || 'mqtt://test.mosquitto.org')
 client.on("connect", () => {console.log("Connected to MQTT Broker!")})
 
 export default client;
