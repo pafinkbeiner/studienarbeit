e5fba0312811a55f321d87269b0c791c9a263c2f
pafinkbeiner <paul-d-f@web.de>
2021-03-23 14:11:22 +0100

    PreMain Continued

--- client/src/pages/PreMain/PreMain.tsx
+++ client/src/pages/PreMain/PreMain.tsx
@@ -1,11 +1,14 @@
-import { IonBadge, IonContent, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonBadge, IonCol, IonContent, IonGrid, IonHeader, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonPage, IonRow, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
 import './PreMain.css';
 import Navbar from '../../components/Navbar/Navbar';
-import { StoreModel } from '../../models/Store';
+import { Sensor, StoreModel } from '../../models/Store';
+import MachineChart from '../Machine/MachineTable/MachineChart';
 
 
 const PreMain: React.FC<{storeModel:StoreModel}> = (props) => {
+
+  const [selectedSensor, setSelectedSensor] = useState<Sensor>();
 
   return (
     <IonPage>
@@ -18,38 +21,80 @@ const PreMain: React.FC<{storeModel:StoreModel}> = (props) => {
             <IonTitle size="large">Predictive Maintenance</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+<IonGrid>
+  <IonRow>
+    <IonCol size="12" sizeLg="6" style={{backgroundColor: "", height: "46.5vh", marginBottom: "0.5vh"}}>
+      {/* Column 1 - Machine Overview */}
         
         <IonList>
 
-          <IonListHeader>Choose a Machine</IonListHeader>
+        <IonListHeader>Choose a Machine</IonListHeader>
 
-          {
-            props.storeModel.machines && 
-            props.storeModel.machines.map((machine) => {
-              return (
-                <IonItem>
-                  <IonLabel>{machine.name}</IonLabel>
-                  <IonBadge color="primary" slot="end">{machine.sensors.length}</IonBadge>
-                  {
-                    machine.sensors && machine.sensors.length > 0 &&
-                      <IonSelect value={machine.sensors} placeholder="Selsect a Sensor" onIonChange={() => {}}>
-                      {
-                        machine.sensors.map(sensor => {
-                          return (
-                            <IonSelectOption value="female">{sensor.name}</IonSelectOption>
-                          )
-                        })
+        {
+          props.storeModel.machines && 
+          props.storeModel.machines.map((machine) => {
+            return (
+              <IonItem>
+                <IonLabel>{machine.name}</IonLabel>
+                <IonBadge color="primary" slot="end">{machine.sensors.length}</IonBadge>
+                {
+                  machine.sensors && machine.sensors.length > 0 &&
+                    <IonSelect value={machine.sensors} placeholder="Selsect a Sensor" onIonChange={(e) => {
+                      let sensor: Sensor|undefined = props.storeModel.machines.find(m => m.id === machine.id)?.sensors.find(s => s.id === e.detail.value);
+                      if(sensor !== undefined){
+                        setSelectedSensor(sensor);
                       }
-                    </IonSelect>
-                  }
-                </IonItem>
-              )
-            })
-          }
-
-
+                    }}>
+                    {
+                      machine.sensors.map(sensor => {
+                        return (
+                          <IonSelectOption value={sensor.id}>{sensor.name}</IonSelectOption>
+                        )
+                      })
+                    }
+                  </IonSelect>
+                }
+              </IonItem>
+            )
+          })
+        }
 
         </IonList>
+
+    </IonCol>
+    <IonCol size="12" sizeLg="6" style={{backgroundColor: "#1E1E1E", height: "46.5vh", marginBottom: "0.5vh"}}>
+      {/* Column 2 - Log Messages */}
+
+      {
+          (selectedSensor) && 
+          <>
+                <h3>Selected Sensor:</h3>
+                {selectedSensor.name}
+                <p>min: {selectedSensor.min}</p>
+                <p>max: {selectedSensor.max}</p>
+                <p>topic: {selectedSensor.topic}</p>
+          </>
+        }
+
+
+    </IonCol>
+    <IonCol size="12" sizeLg="2" style={ selectedSensor && {backgroundColor: "#1E1E1E" , height: "46vh"}}>
+
+        {/* Selection Box with Predictive Maintenance Algoithm */}
+        {/* Component wich renders out the predictive schnittpunkt from a given sensor */}
+
+    </IonCol>
+    <IonCol size="12" sizeLg="10" style={ selectedSensor && {backgroundColor: "#1E1E1E" , height: "46vh"}}>
+      {/* Column 4 - Sensor table */}
+        {
+          (selectedSensor) && 
+            <MachineChart values={selectedSensor.values} min={selectedSensor.min} max={selectedSensor.max}></MachineChart>
+        }
+
+    </IonCol>
+  </IonRow>
+</IonGrid>
 
 
       </IonContent>
