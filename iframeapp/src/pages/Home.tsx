56de9f6aaec6bdf43f10fd5b9fa3e22c73a464cc
pafinkbeiner <paul-d-f@web.de>
2021-03-31 19:43:16 +0200

    added iframe app

---
+++ iframeapp/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+
+        
+          <iframe src="http://studienarbeit-client.paul-finkbeiner.de/" width="100%" height="100%"></iframe>
+
+
+    </IonPage>
+  );
+};
+
+export default Home;
